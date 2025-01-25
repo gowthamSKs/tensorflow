811be6944144d83f24cac285ef5c30c1279f57a5
Toli Yevtushenko <anatoliyy@google.com>
2025-01-25 10:17:33 -0800

    Reduce code duplication in collective_permute_valid_iteration_annotator_test Pointer overload for RunAndCheckHloRewrite.
    PiperOrigin-RevId: 719670477

--- third_party/xla/xla/service/gpu/transforms/collective_permute_valid_iteration_annotator.cc
+++ third_party/xla/xla/service/gpu/transforms/collective_permute_valid_iteration_annotator.cc
@@ -12,15 +12,36 @@ limitations under the License.
 
 #include "xla/service/gpu/transforms/collective_permute_valid_iteration_annotator.h"
 
+#include <algorithm>
+#include <cstddef>
+#include <cstdint>
+#include <optional>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "absl/container/flat_hash_set.h"
+#include "absl/log/check.h"
+#include "absl/log/log.h"
+#include "absl/status/statusor.h"
+#include "absl/strings/str_cat.h"
+#include "absl/strings/str_join.h"
+#include "absl/strings/string_view.h"
 #include "xla/hlo/analysis/while_loop_analysis.h"
+#include "xla/hlo/ir/hlo_instruction.h"
+#include "xla/hlo/ir/hlo_module.h"
+#include "xla/hlo/ir/hlo_opcode.h"
 #include "xla/literal_util.h"
 #include "xla/service/collective_ops_utils.h"
 #include "xla/service/pattern_matcher.h"
+#include "xla/service/source_target_pairs.h"
+#include "xla/tsl/platform/statusor.h"
 
 namespace xla {
 
+using CycleType = SourceTargetPairs::CycleType;
+
 // Finds and returns the non-constant operand in instr.
-//
 // CHECK-fails if instr doesn't have exactly one unique non-constant operand.
 static const HloInstruction* NonConstantOperand(const HloInstruction* instr) {
   const HloInstruction* result = nullptr;
